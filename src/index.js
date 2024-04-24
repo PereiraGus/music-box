88d77c7791e2d9a5ee7b126d5234f6fafb3c35d2
PereiraGus <gustavo.oito@hotmail.com>
2024-04-23 21:41:48 -0300

    Adição da tela de músicas

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import App from './App';
+import Home from './pages/home/Home'
+import Musicas from './pages/musicas/Musicas';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import "./utils/globals.css"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
+    {/* <Musicas /> */}
   </React.StrictMode>
 );
